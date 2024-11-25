3960611d7dc5b8fae5e12c935d3461b552c17dfd
ranggasatria1323 <ranggasatria1323@gmail.com>
2024-11-25 10:16:13 +0700

    add landing-page

---
+++ src/components/Demo/Demo.tsx
@@ -0,0 +1,10 @@
+import { Button, HStack } from "@chakra-ui/react";
+
+export default function Demo(){
+    return(
+        <HStack>
+            <Button bgColor={"lightblue"}>Click Me</Button>
+            <Button>Click Me</Button>
+        </HStack>
+    )
+}
